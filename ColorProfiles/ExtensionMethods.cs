c8906e2548aa85f2a96ec07a521fd0bdd9182da1
Sebastian Safin <sebastian.safin1@gmail.com>
2018-12-10 14:23:19 +0100

    Added Extension Method class

--- ColorProfiles/ExtensionMethods.cs
+++ ColorProfiles/ExtensionMethods.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
 using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace ColorProfiles
@@ -8,6 +11,12 @@ namespace ColorProfiles
         public static Vector ToVector(this Color color)
         {
             return DenseVector.OfArray(new double[] { color.R, color.G, color.B });
+        }
+
+        public static IEnumerable<T> FindAllChildrenByType<T>(this Control control)
+        {
+            IEnumerable<Control> controls = control.Controls.Cast<Control>();
+            return controls.OfType<T>().Concat(controls.SelectMany(ctrl => FindAllChildrenByType<T>(ctrl)));
         }
     }
 }
